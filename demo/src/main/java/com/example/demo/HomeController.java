0857167c9b4592eef70069dfcb98a5f0aed8d97d
Khrixtar <CETECOM@AOAWSB02LC0622.duoc.acad>
2025-04-16 10:42:10 -0400

    primer comentario

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class HomeController {
+    @GetMapping("/")
+    public String index(@RequestParam(defaultValue = "visitante") String nombre){
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-mm-yyyy HH:mm:ss");
+
+        return "<html>" + "<body style = 'font-family:Arial'>" + "<h1> ¡Hola" + nombre + "</h1>" + "<p> Este es nuestro primero proyecto con SpringBoot </p>" +
+         "<p> La hora de creacione es: " + ahora.format(formato) + "</body>" + "</html>";
+    }
+}
